963bf9b07fb1eecc63df60a1738eb46597e16064
MariusVp <mvp200488@gmail.com>
2025-11-11 18:39:56 +0100

    LAgt til stein saks papir

--- Eksempler/Javascript/javascript.js
+++ Eksempler/Javascript/javascript.js
@@ -1,2 +1,15 @@
-let navn = "Marius";
-console.log(navn[0]); // Vil skrive ut M
+let handleliste = ["melk", "brød", "ost", "smør"];
+
+function fjernVare(vare) {
+  let indeks = handleliste.indexOf(vare);
+  if (indeks !== -1) {
+    handleliste.splice(indeks, 1);
+    console.log(`${vare} er fjernet fra handlelista 🗑️`);
+  } else {
+    console.log(`${vare} finnes ikke i lista`);
+  }
+}
+
+fjernVare("ost");
+fjernVare("egg");
+console.log(handleliste);
